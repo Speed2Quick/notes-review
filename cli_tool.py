ee5a8cca31a2b4971c02423229a950744efaa62c
Speed2Quick <4samuelr05@gmail.com>
2026-04-22 19:21:22 -0400

    feat: implement state machine for controlling main logic

--- cli_tool.py
+++ cli_tool.py
@@ -1,17 +1,27 @@
-from sqlite3 import connect
-from app.database import get_connection, initialize_db, create_deck, delete_deck, print_table, create_card, delete_card, get_card, get_deck, update_next_review, get_cards_for_review, get_num_cards_in_deck
-from sm2_logic import calculate_sm2
-import datetime
-
+from app.database import get_connection, initialize_db
+from interface import choose_menu_action, choose_deck_or_card, choose_deck, get_card_info, add_cards
+from states import Context, State
 
 def main():
 
     connection = get_connection()
+    ctx = Context(conn=connection)
+
+    #maps the state to functions
+    state_map: dict = {
+            State.MAIN_MENU: choose_menu_action,
+            State.SELECT_ADD: choose_deck_or_card,
+            State.ADD_CARDS: add_cards
+            }
 
     try:
 
         initialize_db(connection)
+        current_state = State.MAIN_MENU
 
+        while current_state != State.EXIT:
+            handler_function = state_map[current_state]
+            current_state =  handler_function(ctx)
 
     finally:
         connection.close()
---
+++ states.py
@@ -0,0 +1,18 @@
+import sqlite3
+from enum import Enum, auto
+from dataclasses import dataclass
+
+#tracks the state of the program
+class State(Enum):
+    MAIN_MENU = auto()
+    SELECT_ADD = auto()
+    ADD_CARDS = auto()
+    EXIT = auto()
+
+#stores data to be passed into functions held by state
+@dataclass
+class Context:
+    conn: sqlite3.Connection
+    deck_id = None
+    card_id = None
+
